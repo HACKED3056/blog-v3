1caddb87cfb0dce9b0bd0ed5d6b6000b9e2968c9
HACKED3056 <3255154997@qq.com>
2026-06-05 23:01:41 +0800

    fix: navbar icon now uses favicon instead of QQ avatar

--- app/app.config.ts
+++ app/app.config.ts
@@ -91,7 +91,7 @@ export default defineAppConfig({
 
 	/** 左侧栏顶部 Logo */
 	header: {
-		logo: blogConfig.author.avatar,
+		logo: blogConfig.favicon,
 		/** 展示标题文本，否则展示纯 Logo */
 		showTitle: true,
 		subtitle: blogConfig.subtitle,
