22821c6ba194b461ee9e35cb7cc6e6ed71811eaf
HACKED3056 <3255154997@qq.com>
2026-07-15 18:26:49 +0800

    fix: BlogStats last update now matches latest article update

--- app/components/widget/BlogStats.vue
+++ app/components/widget/BlogStats.vue
@@ -1,34 +1,41 @@
 <script setup lang="ts">
-import { UtilDate } from #components
+import { UtilDate } from '#components'
 
 const appConfig = useAppConfig()
 const runtimeConfig = useRuntimeConfig()
-const articleUpdate = useNuxtData('widget:article-update')
 
 // 响应头不正确时，stats.value 可能会是字符串，首次属性访问可能为 undefined
-const { data: stats } = useFetch(/api/stats)
+const { data: stats } = useFetch('/api/stats')
+const { data: lastUpd } = await useAsyncData('blog:last-upd', () =>
+  queryCollection('content')
+    .where('stem', 'LIKE', 'posts/%')
+    .order('updated', 'DESC')
+    .limit(1)
+    .all()
+    .then(r => r[0]?.updated || '')
+)
 
 const yearlyTip = computed(() => Object
 	.entries(stats.value?.annual || {})
 	.reverse()
-	.map(([year, item]) => )
-	.join(n) || 数据获取失败,
+	.map(([year, item]) => `${year}年：${item.posts}篇，${formatNumber(item.words)}字`)
+	.join('\n') || '数据获取失败',
 )
 
 const blogStats = [{
-	label: 运营时长,
+	label: '运营时长',
 	value: timeElapse(appConfig.timeEstablished),
-	tip: ,
+	tip: `博客于${appConfig.timeEstablished}上线`,
 }, {
-	label: 上次更新,
+	label: '上次更新',
 	value: () => h(UtilDate, {
-		date: articleUpdate.data.value?.[0]?.updated || runtimeConfig.public.buildTime,
+		date: lastUpd.value || runtimeConfig.public.buildTime,
 		relative: true,
-		tipPrefix: articleUpdate.data.value?.[0]?.updated ? "" : "构建于",
+		tipPrefix: lastUpd.value ? '' : '构建于',
 	}),
 }, {
-	label: 总字数,
-	value: computed(() => formatNumber(stats.value?.total?.words) || --),
+	label: '总字数',
+	value: computed(() => formatNumber(stats.value?.total?.words) || '--'),
 	tip: yearlyTip,
 }]
 </script>
